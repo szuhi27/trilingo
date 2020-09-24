112a5cafc7a414ae7798005b7459a20af5286431
Thryms <43387823+Thryms@users.noreply.github.com>
2020-09-24 10:35:25 +0200

    fxml declarations added

--- src/main/java/trilingo/controllers/Menu.java
+++ src/main/java/trilingo/controllers/Menu.java
@@ -14,18 +14,29 @@ import javafx.scene.layout.VBox;
 public class Menu {
 
     @FXML
-    public VBox languagesVB, difficultiesVB;
+    public Pane gameP, languagesP, difficultiesP;
     @FXML
-    public Pane gameP;
+    public ImageView questionIV, bcgIV, resultIV, answ1IV, answ2IV, answ3IV, answ4IV, engIV, russIV, finnIV,
+            easyIV, mediumIV, proIV, menuIV, muteIV, exitIV;
     @FXML
-    public ImageView questionIV;
+    public Button answ1B, answ2B, answ3B, answ4B;
+    @FXML
+    public Text resultT;
+    @FXML
+    public StackPane resultSP;
 
-    private String language, difficulty;
+    private String language, chosenDiff;
+    private String[] answ1, answ2, answ3, answ4, correctAnsw, images;
+    private int[] previousQuestions;
 
+    private int numberOfQuestionsAsked, questionNumber, numberOfQuestionsAvailable = 0, goodAnswers;
+    MediaPlayer a;
+    private boolean music=true;
 
     @FXML
     public void initialize() {
-
+        LoadImages();
+        music();
     }
 
     public void ChooseLanguage(ActionEvent event){
